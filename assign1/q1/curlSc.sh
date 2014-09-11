82ce8fcdbd5d9614886658120104fbbaca98d370
george micros <gmicr001@odu.edu>
2014-09-11 18:37:41 -0400

    e commit message for your changes. Lines starting

--- assign1/q1/curlSc.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-rm -f *.htm
-#curl -v -X POST -d "name=george" -d @gmicrosTest.txt http://www.posttestserver.com/post.php > postTest.htm
-#curl -v -X POST -d "name=george" -d @gmicrosTest.txt http://httpbin.org/post > httpBin.htm
-curl -v -X POST -d "name=george" -d @gmicrosTest.txt http://greensuisse.zzl.org/product/dump/dump.php > green.htm
-
-
